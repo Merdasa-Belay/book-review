b8822893b02499e9b9054ac5b92fb1c507af3097
Merdasa-Belay <merdasabelay@gmail.com>
2024-09-15 14:19:45 +0300

    update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,7 +28,9 @@ class BookController extends Controller
             'highest_rated_last_6month' => $books->highestRated6LastMonth(),
             default => $books->latest()
         };
-        $books = $books->get();
+        // $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
